2b5795b6ce47178e768c32105686a855c433d2cf
Zaridigi <zaridigi@gmail.com>
2025-11-19 01:39:36 +0300

    tagmanager

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 export const articles = [{
   id: 0, // Used in JSX as a key
-  url_img: 'https://ucare.timepad.ru/74469933-39e7-49a1-9e2a-ee70a99597fb/poster_event_2908750.jpg',
+  url_img: 'https://avatars.mds.yandex.net/i?id=d5e939566e27e25f16b9fd39f6d562d56732bbf1-5235257-images-thumbs&n=13',
   title: 'Основы Web-разработки',
   discription: 'spaceflight calculations',
   url: 'https://gzmland.ru/'
 }, {
 id: 0, // Used in JSX as a key
-  url_img: 'https://ucare.timepad.ru/74469933-39e7-49a1-9e2a-ee70a99597fb/poster_event_2908750.jpg',
+  url_img: 'https://s3.amazonaws.com/media-p.slid.es/uploads/1150869/images/7408329/2560px-React-icon.svg.png',
   title: 'React для начинающих',
   discription: 'spaceflight calculations',
   url: 'https://gzmland.ru/'
 }, {
 id: 0, // Used in JSX as a key
-  url_img: 'https://ucare.timepad.ru/74469933-39e7-49a1-9e2a-ee70a99597fb/poster_event_2908750.jpg',
+  url_img: 'https://avatars.githubusercontent.com/u/148135964?v=4?s=400',
   title: 'Работа с Git',
   discription: 'spaceflight calculations',
   url: 'https://gzmland.ru/'
